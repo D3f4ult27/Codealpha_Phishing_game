b867c859e8884ed546d89396115ea6cd8538d04f
D3f4ult27 <brownenock27@gmail.com>
2025-10-24 16:51:12 +0300

    Update app to use environment variable for port

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify, session
 import json
+import os
 from datetime import datetime
 
 app = Flask(__name__)
@@ -263,5 +264,6 @@ def analyze_website():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
 
